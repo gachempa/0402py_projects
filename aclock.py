6797ba644a389c86c7ed07b06c3da70f8bbb985c
gachempa <jpsg514@gmail.com>
2022-04-02 20:41:35 -0700

    added welcome message

--- aclock.py
+++ aclock.py
@@ -1,7 +1,12 @@
+from cProfile import label
 from  tkinter import Tk, Label
 
 window = Tk()
 window.title("Clock")
-window.geometry("300x200")
+window.geometry("600x300")
+window.configure(bg="steelblue")
+
+label = Label(window, text="Welcome", font=("Arial Black",30,"bold"), bg="blue", fg="yellow")
+label.pack(pady=100)
 
 window.mainloop()
