7af17c99ce35a5a65e338300e9c09fda1cee976f
gachempa <jpsg514@gmail.com>
2022-04-09 19:06:42 -0700

    fixed main.py

--- py_folder/main.py
+++ py_folder/main.py
@@ -1,6 +1,17 @@
-from TestMain import user_info
+# the code block below is for collecting user information and printing
 
-NumberOfUsers = int(input("How many users do you want to collect the information from?"))
-OurUserList = user_info(NumberOfUsers)
-print("Your first name is ", (OurUserList[1])[0])
+from personDataAccess import PersonData
+
+# These are our current members, stored in a text file
+def CurrentMembers():
+    ourUserList1 = PersonData(1)
+    print(ourUserList1)
+
+print("Current members:")
+CurrentMembers()
+
+print("Let's add a user to the text file:")
+PersonData(2)
+CurrentMembers()
+
 
